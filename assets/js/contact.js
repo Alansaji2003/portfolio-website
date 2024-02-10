3e7509ea00113b031083edec3288dd7c783ee4be
Alan Saji <rockstaralansaji@gmail.com>
2024-02-10 17:15:24 +0530

    update

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,24 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+    //serviceId - templateId - #form -publicKey
+    emailjs.sendForm('service_pq09qg7', 'template_5zetpn9', '#contact-form', 'PqJLeJErKDMByV0QK')
+        .then(()=>{
+            contactMessage.textContent = 'Message sent Successfully ✅'
+
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000 )
+
+            //clear input fields
+            contactForm.reset()
+        }, () => {
+            //error message
+            contactMessage.textContent = 'Message not sent (service error)❌'
+        })
+}
+
+contactForm.addEventListener('submit', sendEmail)
